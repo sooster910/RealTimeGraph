67a558c21ef582a8c4d76785c8dd70c1a90bdba4
Hyunsu Joo <hs9880@gmail.com>
2020-11-18 21:04:01 +0900

    Fix wiring redis socket io not responding

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 require('dotenv').config()
 const express = require('express');
-
+const socketio = require('socket.io');
+const net = require('net');
 const io_redis = require('socket.io-redis');
-const farmhash=require('farmhash');
 const cluster = require('cluster');
+const farmhash = require('farmhash');
 const port = process.env.PORT || 9001
 const socketMain = require('./socketMain');
-const socketio = require('socket.io');
+const helmet = require('helmet');
 const os = require('os');
 const numOfProcesses = os.cpus().length;
 const upTime = os.uptime(); //needed?
 const freeMemory = os.freemem();
 const totalMemory = os.totalmem();
-const usedMemory  =  totalMemory - freeMemory;
-const usedMemoryRate = Math.floor((usedMemory/totalMemory)*100)
-
+const usedMemory = totalMemory - freeMemory;
+const usedMemoryRate = Math.floor((usedMemory / totalMemory) * 100)
 
 const cpus = os.cpus();
+
 // for(var i = 0; i < cpus.length; i++) {
 // 	console.log("CPU[" + (i+1) + "]");
 // 	console.log("model: " + cpus[i].model);
@@ -24,57 +25,58 @@ const cpus = os.cpus();
 //     console.log("times:"+ JSON.stringify(cpus[i].times))
 // }
 
-if(cluster.isMaster){
+if (cluster.isMaster) {
     let workers = [];
 
-    let spawn = function(i){
+    let spawn = function (i) {
 
         //grabs paricular fork and calls fork
         workers[i] = cluster.fork();
-        workers[i].on('exit', (code, signal)=>{
-            console.log('respawning worker',i);
+        workers[i].on('exit', (code, signal) => {
+            console.log('respawning worker', i);
             spawn(i);
-        }) 
+        })
     }
 
-for(let i=0;i<numOfProcesses;i++){
-    spawn(i);
-}
-const worker_index =(ip,len)=>{
-    return farmhash.fingerprint32(ip)%len;
+    for (let i = 0; i < numOfProcesses; i++) {
+        spawn(i);
+    }
+    const worker_index = (ip, len) => {
+        return farmhash.fingerprint32(ip) % len;
 
-}
+    }
 
-const server = net.createServer({ pauseOnConnect: true }, (connection) =>{
-    // We received a connection and need to pass it to the appropriate
-    // worker. Get the worker for this connection's source IP and pass
-    // it the connection.
-    let worker = workers[worker_index(connection.remoteAddress, num_processes)];
-    worker.send('sticky-session:connection', connection);
-});
-server.listen(port);
-console.log(`Master listening on port ${port}`);
-}else{
+    const server = net.createServer({ pauseOnConnect: true }, (connection) => {
+
+        let worker = workers[worker_index(connection.remoteAddress, numOfProcesses)];
+        worker.send('sticky-session:connection', connection);
+    });
+    server.listen(port);
+    console.log(`Master listening on port ${port}`);
+} else {
     const app = express();
+    app.use(express.static(__dirname + '/public'));
+    app.use(helmet());
     const server = app.listen(0, 'localhost');
     const io = socketio(server);
 
     //redis adapter configure 
-    io.adapter(io_redis({host:'localhost',port:6347}));
-
+    io.adapter(io_redis({ host: 'localhost', port: 6379 }));
+    server.listen(0, 'localhost', () => {
+        console.log(`connected to worker: ${cluster.worker.id}`)
+    });
     //connect socke to node client
-    io.on('connection',(socket)=>{
+    io.on('connection', (socket) => {
         socketMain(io, socket);
+        console.log(`connected to worker: ${cluster.worker.id}`);
     });
 
-    // socketMain(io,null);
 
-    process.on('message',(message, connection)=>{
-        if(message !== 'sticky-session:connection'){
+    process.on('message', (message, connection) => {
+        if (message !== 'sticky-session:connection') {
             return;
         }
+        server.emit('connection', connection);
 
-        server.emit('connection'.connection);
-        
     })
 }
